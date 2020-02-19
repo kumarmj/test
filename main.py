76776ed5da5589859e4d41432625a67a66dbc679
Manoj Kumar <iKumarMJ@gmail.com>
2020-02-19 07:45:17 +0000

    changes

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+# Chapter 1: The role of algorithms in Computing
+# In this chapter, we will answer these questions
+
+# What are algorithms?
+# An algorithm is thus a sequence of computational steps that transform the input into the output
+
+# e.g. let's define sorting problem
+# Input: A sequence of n numbers a1, a2,...an.
+# Output: Produce a reordering where a1 < a2 < .. < an 
+
+nums = [2, 1, 5, 4, 3]
+print("Input: ", end='')
+print(nums)
+print("Problem: Produce ordering where a1 < a2 < .. < an")
+print("Output: ", end='')
+print(sorted(nums))
+
+# Which algorithm is best for a given application depends on—among other factors—
+# - the number of items to be sorted, the extent to which the items are already somewhat sorted, possible restrictions on the item values,
+# - the architecture of the computer, and the kind of storage devices to be used: main memory, disks, or even tapes.
+
+# Why is the study of algorithms worthwhile ?
+
+# What is the role of algorithms relative to other technologies used in computers ?
